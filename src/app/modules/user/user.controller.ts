96d89bdb67e6b5114c510b4598e6d6b29586e23a
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-07-08 22:57:06 +0600

    26-6

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { User } from "./user.model";
+import { UserServices } from "./user.service";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await UserServices.createUser(req.body);
+    res.status(httpStatus.CREATED).json({
+      message: "User Created Successfully",
+      user,
+    });
+  } catch (err: any) {
+    console.log(err);
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: `Something Went Wrong!! ${err.message}`,
+    });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
+
+// route matching -> controller -> service -> model -> DB
