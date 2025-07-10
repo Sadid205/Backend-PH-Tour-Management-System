e684dce3b613b41e561e4365644f5609d8ff0587
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-07-10 23:07:33 +0600

    27-3

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
+import { validateRequest } from "../../middlewares/validatedRequest";
+import { createUserZodSchema } from "./user.validationl";
 
 const router = Router();
 
-router.post("/register", UserControllers.createUser);
+router.post("/register",validateRequest(createUserZodSchema), UserControllers.createUser);
 router.get("/all-users",UserControllers.getAllUsers)
 export const UserRoutes = router;
