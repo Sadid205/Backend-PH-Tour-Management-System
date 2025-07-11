3b18a931dd9a33d9e1874b0ccbea4f5a34556c88
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-07-11 09:33:57 +0600

    27-6

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import AppError from "../../errorHelpers/AppErrors";
+import { User } from "../user/user.model";
+import httpStatus from "http-status-codes";
+import bcryptjs from "bcryptjs";
+import { IUser } from "../user/user.interface";
+const credentialsLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
+
+  const isUserExist = await User.findOne({ email });
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
+  }
+  const isPasswordMatched = await bcryptjs.compare(
+    password as string,
+    isUserExist.password as string
+  );
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password");
+  }
+  return {
+    email: isUserExist.email,
+  };
+};
+
+export const AuthServices = {
+  credentialsLogin,
+};
